726bcad44f3466debf115ca9923e7b5d07687996
Gantong <eakphisitdha.u@komgrip.com>
2024-07-19 17:01:29 +0700

    add general function

---
+++ general_function/print_to_json.go
@@ -0,0 +1,13 @@
+package gotool
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/tidwall/pretty"
+)
+
+func PrintToJson(v ...interface{}) {
+	bytes, _ := json.MarshalIndent(v, "", "\t")
+	fmt.Println(string(pretty.Color(pretty.PrettyOptions(bytes, pretty.DefaultOptions), nil)))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gotool
+
+go 1.21.5
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/tidwall/pretty v1.2.1
+)
